d47c1052627963d754d73798cba46fc3ee8a7a9f
Brady Love <love.brady@gmail.com>
2017-08-21 15:17:28 -0600

    Initial commit
    [#148451433]

---
+++ client.go
@@ -0,0 +1,196 @@
+package loggingclient
+
+import (
+	"fmt"
+	"time"
+
+	loggregator "code.cloudfoundry.org/go-loggregator"
+	"code.cloudfoundry.org/go-loggregator/v1"
+	"github.com/cloudfoundry/sonde-go/events"
+)
+
+// Config is the shared configuration between v1 and v2 clients.
+type Config struct {
+	UseV2API      bool   `json:"loggregator_use_v2_api"`
+	APIPort       int    `json:"loggregator_api_port"`
+	CACertPath    string `json:"loggregator_ca_path"`
+	CertPath      string `json:"loggregator_cert_path"`
+	KeyPath       string `json:"loggregator_key_path"`
+	JobDeployment string `json:"loggregator_job_deployment"`
+	JobName       string `json:"loggregator_job_name"`
+	JobIndex      string `json:"loggregator_job_index"`
+	JobIP         string `json:"loggregator_job_ip"`
+	JobOrigin     string `json:"loggregator_job_origin"`
+
+	BatchMaxSize       uint
+	BatchFlushInterval time.Duration
+}
+
+// IngressClient is the shared contract between v1 and v2 clients.
+type IngressClient interface {
+	SendDuration(name string, value time.Duration) error
+	SendMebiBytes(name string, value int) error
+	SendMetric(name string, value int) error
+	SendBytesPerSecond(name string, value float64) error
+	SendRequestsPerSecond(name string, value float64) error
+	IncrementCounter(name string) error
+	IncrementCounterWithDelta(name string, value uint64) error
+	SendAppLog(appID, message, sourceType, sourceInstance string) error
+	SendAppErrorLog(appID, message, sourceType, sourceInstance string) error
+	SendAppMetrics(metrics *events.ContainerMetric) error
+	SendComponentMetric(name string, value float64, unit string) error
+}
+
+// NewIngressClient returns a v1 or v2 client depending on the value of `UseV2API`
+// from the config
+func NewIngressClient(config Config) (IngressClient, error) {
+	if config.UseV2API {
+		return newV2IngressClient(config)
+	}
+
+	return newV1IngressClient(config)
+}
+
+// NewV1IngressClient creates a V1 connection to the Loggregator API.
+func newV1IngressClient(config Config) (IngressClient, error) {
+	c, err := v1.NewClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return client{client: c}, nil
+}
+
+// NewV2IngressClient creates a V2 connection to the Loggregator API.
+func newV2IngressClient(config Config) (IngressClient, error) {
+	tlsConfig, err := loggregator.NewIngressTLSConfig(
+		config.CACertPath,
+		config.CertPath,
+		config.KeyPath,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	opts := []loggregator.IngressOption{
+		// Whereas Metron will add tags for deployment, name, index, and ip,
+		// it does not add job origin and so we must add it manually here.
+		loggregator.WithTag("origin", config.JobOrigin),
+	}
+
+	if config.BatchMaxSize != 0 {
+		opts = append(opts, loggregator.WithBatchMaxSize(config.BatchMaxSize))
+	}
+
+	if config.BatchFlushInterval != time.Duration(0) {
+		opts = append(opts, loggregator.WithBatchFlushInterval(config.BatchFlushInterval))
+	}
+
+	if config.APIPort != 0 {
+		opts = append(opts, loggregator.WithAddr(fmt.Sprintf("localhost:%d", config.APIPort)))
+	}
+
+	c, err := loggregator.NewIngressClient(tlsConfig, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return client{client: c}, nil
+}
+
+type logClient interface {
+	EmitLog(msg string, opts ...loggregator.EmitLogOption)
+	EmitGauge(opts ...loggregator.EmitGaugeOption)
+	EmitCounter(name string, opts ...loggregator.EmitCounterOption)
+}
+
+type client struct {
+	client logClient
+}
+
+func (c client) SendDuration(name string, value time.Duration) error {
+	c.client.EmitGauge(
+		loggregator.WithGaugeValue(name, float64(value), "nanos"),
+	)
+	return nil
+}
+
+func (c client) SendMebiBytes(name string, value int) error {
+	c.client.EmitGauge(
+		loggregator.WithGaugeValue(name, float64(value), "MiB"),
+	)
+	return nil
+}
+
+func (c client) SendMetric(name string, value int) error {
+	c.client.EmitGauge(
+		loggregator.WithGaugeValue(name, float64(value), "Metric"),
+	)
+
+	return nil
+}
+
+func (c client) SendBytesPerSecond(name string, value float64) error {
+	c.client.EmitGauge(
+		loggregator.WithGaugeValue(name, value, "B/s"),
+	)
+	return nil
+}
+
+func (c client) SendRequestsPerSecond(name string, value float64) error {
+	c.client.EmitGauge(
+		loggregator.WithGaugeValue(name, value, "Req/s"),
+	)
+	return nil
+}
+
+func (c client) IncrementCounter(name string) error {
+	c.client.EmitCounter(name)
+
+	return nil
+}
+
+func (c client) IncrementCounterWithDelta(name string, value uint64) error {
+	c.client.EmitCounter(name, loggregator.WithDelta(value))
+
+	return nil
+}
+
+func (c client) SendAppLog(appID, message, sourceType, sourceInstance string) error {
+	c.client.EmitLog(
+		message,
+		loggregator.WithAppInfo(appID, sourceType, sourceInstance),
+		loggregator.WithStdout(),
+	)
+	return nil
+}
+
+func (c client) SendAppErrorLog(appID, message, sourceType, sourceInstance string) error {
+	c.client.EmitLog(
+		message,
+		loggregator.WithAppInfo(appID, sourceType, sourceInstance),
+	)
+	return nil
+}
+
+func (c client) SendAppMetrics(m *events.ContainerMetric) error {
+	c.client.EmitGauge(
+		loggregator.WithGaugeValue("instance_index", float64(m.GetInstanceIndex()), ""),
+		loggregator.WithGaugeValue("cpu", m.GetCpuPercentage(), "percentage"),
+		loggregator.WithGaugeValue("memory", float64(m.GetMemoryBytes()), "bytes"),
+		loggregator.WithGaugeValue("disk", float64(m.GetDiskBytes()), "bytes"),
+		loggregator.WithGaugeValue("memory_quota", float64(m.GetMemoryBytesQuota()), "bytes"),
+		loggregator.WithGaugeValue("disk_quota", float64(m.GetDiskBytesQuota()), "bytes"),
+		loggregator.WithGaugeAppInfo(m.GetApplicationId()),
+	)
+
+	return nil
+}
+
+func (c client) SendComponentMetric(name string, value float64, unit string) error {
+	c.client.EmitGauge(
+		loggregator.WithGaugeValue(name, value, unit),
+	)
+
+	return nil
+}
